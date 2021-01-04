1d46bcf8e80d6b5dc68afe6c324f4ba7f58252e3
Michael DeGori <michael.degori@gmail.com>
2021-01-04 10:34:30 -0500

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,22 @@
-import '../styles/Contact.css'
+import '../styles/Contact.css';
 import React from 'react';
 
 const Contact = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="contact">
+      <div id="contact-title">
+        <div id="contact-title-container">
+          <h1>Contact me</h1>
+          <h3>Get in touch!</h3>
+        </div>
+      </div>
+
+      <div id="contact-section">
+        <div className="contact-container">
+          <h3>Hey there!</h3>
+        </div>
+      </div>
+    </section>
   );
 };
 
